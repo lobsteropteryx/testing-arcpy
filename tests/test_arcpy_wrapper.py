a57178c8a2c90c402fa967a59d093ca32dd6e211
ifirkin <ian.firkin@homeinsteadinc.com>
2017-07-04 23:09:38 -0400

    Adds dupe test

--- tests/test_arcpy_wrapper.py
+++ tests/test_arcpy_wrapper.py
@@ -1,20 +1,31 @@
 from unittest import TestCase
 from mock import patch, MagicMock
-from my_project.arcpy_wrapper import list_workspaces_for_mxd
-
+from arcpy_wrapper import list_workspaces_for_mxd
 
 @patch('my_project.arcpy_helper.arcpy')
 class TestListDataSources(TestCase):
+
+    def create_mock_layer(self, workspace):
+        layer = MagicMock()
+        layer.supports = MagicMock(return_value=True)
+        layer.workspacePath = workspace
+        return layer
+
     def test_list_single_data_source(self, mock_arcpy):
         data_source = 'layer1'
-
-        layer = MagicMock()
-        layer.supports.return_value = True
-        layer.workspacePath = data_source
-
+        layer = self.create_mock_layer(data_source)
         mock_arcpy.mapping.ListLayers = MagicMock(return_value=[layer])
         mxd = {}
         expected = [data_source]
         actual = list_workspaces_for_mxd(mxd)
+        self.assertEqual(expected, actual)
 
+    def test_lists_unique_data_sources(self, mock_arcpy):
+        data_source1 = 'layer1'
+        data_source2 = 'layer2'
+        layer1 = self.create_mock_layer(data_source1)
+        layer2 = self.create_mock_layer(data_source2)
+        mock_arcpy.mapping.ListLayers = MagicMock(return_value=[layer1, layer1, layer2])
+        expected = sorted([data_source2, data_source1])
+        actual = sorted(list_workspaces_for_mxd({}))
         self.assertEqual(expected, actual)
